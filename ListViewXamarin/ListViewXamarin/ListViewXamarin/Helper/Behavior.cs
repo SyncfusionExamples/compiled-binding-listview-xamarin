c2dd40f380232fb5c3d24d585f23f11cf197ec12
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-27 11:36:54 +0530

    XAMARIN-30545 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        SfListView ListView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            ListView.DataSource.SortDescriptors.Add(new SortDescriptor()
+            {
+                PropertyName = "ContactName",
+                Direction = ListSortDirection.Ascending
+            });
+            ListView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+            {
+                PropertyName = "ContactName",
+                KeySelector = (object obj1) =>
+                {
+                    var item = (obj1 as Model.Contacts);
+                    return item.ContactName[0].ToString();
+                },
+            });
+            base.OnAttachedTo(bindable);
+        }
+    }
+}
